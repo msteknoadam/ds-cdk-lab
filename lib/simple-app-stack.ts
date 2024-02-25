74bab3d192fa183e6d9837e89878ea4b5c9e2ac3
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2024-02-25 17:18:29 +0000

    Lambda function to get all movies.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -74,5 +74,28 @@ export class SimpleAppStack extends cdk.Stack {
 		moviesTable.grantReadData(getMovieByIdFn);
 
 		new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
+
+		const getAllMovies = new lambdanode.NodejsFunction(this, "GetAllMovies", {
+			architecture: lambda.Architecture.ARM_64,
+			runtime: lambda.Runtime.NODEJS_16_X,
+			entry: `${__dirname}/../lambdas/getAllMovies.ts`,
+			timeout: cdk.Duration.seconds(10),
+			memorySize: 128,
+			environment: {
+				TABLE_NAME: moviesTable.tableName,
+				REGION: "eu-west-1",
+			},
+		});
+
+		const getAllMoviesURL = getAllMovies.addFunctionUrl({
+			authType: lambda.FunctionUrlAuthType.NONE,
+			cors: {
+				allowedOrigins: ["*"],
+			},
+		});
+
+		moviesTable.grantReadData(getAllMovies);
+
+		new cdk.CfnOutput(this, "Get All Movies Function Url", { value: getAllMoviesURL.url });
 	}
 }
